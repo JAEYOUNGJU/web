25cf1f058aaaea0483f71369402ecacc72bffc34
jujaeyoung <jujy4315@gmail.com>
2022-06-09 15:12:36 +0900

    수정중

---
+++ j/Algo2.java
@@ -0,0 +1,22 @@
+package j;
+
+import java.util.Scanner;
+
+public class Algo2 {
+//문자열로 표준 입력을 통해 입력받기
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int R = scanner.nextInt();
+
+		int N = scanner.nextInt();
+
+		int[] x = new int[N];
+		int[] y = new int[N];
+	
+	}
+
+}
+
