3b815dd098794158ee889ad50d2d264dfe5e978a
jujaeyoung <jujy4315@gmail.com>
2022-06-09 15:14:12 +0900

    수정중

--- j/Algo2.java
+++ j/Algo2.java
@@ -3,10 +3,11 @@ package j;
 import java.util.Scanner;
 
 public class Algo2 {
-//문자열로 표준 입력을 통해 입력받기
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
+        //문자열로 표준 입력을 통해 입력받기
+        Scanner scanner = new Scanner(System.in);
 		int a = scanner.nextInt();
 		int b = scanner.nextInt();
 		int R = scanner.nextInt();
