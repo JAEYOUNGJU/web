5f8371008028e4b94d4709296525a91d3fd0fa6d
jujaeyoung <jujy4315@gmail.com>
2022-06-09 14:18:32 +0900

    수정

--- j/Algo1.java
+++ j/Algo1.java
@@ -2,6 +2,7 @@ package j;
 
 import java.util.Scanner;
 
+//문자열 결합: 입력받은 문자열 수와 문자열을 입력받고 출력 포멧에 맞게 출력하기
 public class Algo1 {
     // main하고 enter 하면 자동됨
     public static void main(String[] args) {
@@ -9,15 +10,20 @@ public class Algo1 {
         System.out.println(resolve());
         
     }
-
+    
     public static String resolve() {
     	Scanner scanner = new Scanner(System.in);
-    	int text = scanner.nextInt();
-    	String[] text2 = new String[text];
+    	int num = scanner.nextInt();
+    	String[] arr = new String[num];
     	
-    	for (int i = 0; i < text; i++) {
-    		 text2[i] = scanner.next();
+    	for (int i = 0; i < num; i++) {
+    		arr[i] = scanner.next();
     	}
+    	System.err.print("Hello ");
+    	for (int i=0; i < (num-1); i++) {
+    		System.out.print(arr[i]+",");
+    	}
+    	System.out.print(arr[(num-1)]+".");
     	return "";
     
         
