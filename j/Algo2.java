38287cc8576474a17b4fad9336fcbdcdcf753cfd
jujaeyoung <jujy4315@gmail.com>
2022-06-09 15:19:14 +0900

    진행중

--- j/Algo2.java
+++ j/Algo2.java
@@ -13,10 +13,13 @@ public class Algo2 {
 		int R = scanner.nextInt();
 
 		int N = scanner.nextInt();
-
 		int[] x = new int[N];
 		int[] y = new int[N];
 	
+		
+		for(int i = 0; i < N; i++) {
+			
+		}
 	}
 
 }
