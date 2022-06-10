3638ee18f75bc2019e6add0f92f135718fa302f6
jujaeyoung <jujy4315@gmail.com>
2022-06-10 12:19:57 +0900

    조용한 곳 찾기

--- j/Algo2.java
+++ j/Algo2.java
@@ -1,26 +1,36 @@
 package j;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Algo2 {
+	//Algorithm 2
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        //문자열로 표준 입력을 통해 입력받기
-        Scanner scanner = new Scanner(System.in);
+		ArrayList<String> result = new ArrayList<>();
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("공사장의 좌표와 소음 크기를 입력하세요.");
 		int a = scanner.nextInt();
 		int b = scanner.nextInt();
-		int R = scanner.nextInt();
+		int R = scanner.nextInt(); //반지름
 
+		System.out.println("나무그늘의 수를 입력하세요.");
 		int N = scanner.nextInt();
-		int[] x = new int[N];
-		int[] y = new int[N];
-	
-		
-		for(int i = 0; i < N; i++) {
-			
+
+		for (int i = 0; i < N; i++) {
+
+			int x = scanner.nextInt();
+			int y = scanner.nextInt();
+
+			if (Math.pow((x - a), 2) + Math.pow((y - b), 2) >= Math.pow(R, 2)) {
+				System.out.println("silent");
+			} else {
+				System.out.println("noisy");
+			}
 		}
+		scanner.close();
 	}
 
 }
-
