48460da4bf3e5213c86391bc192770a8dc4e3c99
jujaeyoung <jujy4315@gmail.com>
2022-06-10 12:19:47 +0900

    문자열 결합

--- j/Word.java
+++ j/Word.java
@@ -6,41 +6,36 @@ public class Word {
 		Word word = new Word("Hello");
 		System.out.println(word.isVowel(0));
 		System.out.println(word.isVowel(1));
-
+		System.out.println(word.isVowel(2));
+		System.out.println(word.isVowel(3));
+		System.out.println(word.isVowel(4));
+		System.out.print("\n");
+		System.out.println(word.isConsonant(0));
+		System.out.println(word.isConsonant(1));
+		System.out.println(word.isConsonant(2));
+		System.out.println(word.isConsonant(3));
+		System.out.println(word.isConsonant(4));
+		
 	}
 	
 	private String letters;
 	
-	//String만 가지고 있는것임
+	//String만 가지고 있는 것
 	
-	//마우스 우클릭 -> 
+	//생성자 자동 생성하기
+	//마우스 우클릭 -> Source -> Generate Constructor using Field -> Generate 클릭
 	public Word(String letters) {
 		this.letters = letters.toLowerCase(); //객체 생성하기
 		
 	}
-	//i번째 글자가 모음인지 맞으면 true, 틀리면 false 
-	//a,e,i,o,u가 영어에서 모음임
-	
+	//i번째 글자가 모음(a,e,i,o,u)인지 맞으면 true, 틀리면 false 
+		
 	public boolean isVowel(int i) {
-		return letters.substring(i, i+1).equals("a") 
-				|| letters.substring(i, i+1).equals("e")
-				|| letters.substring(i, i+1).equals("i") 
-				|| letters.substring(i, i+1).equals("o") 
-				|| letters.substring(i, i+1).equals("u");	
-		}
-	public boolean isVowel2(int i) {
-		String vowel = "aeiou";
-		return ((String) letters.subSequence(i, i+1)).contains(vowel);
-			
+		return "aeiou".contains(letters.substring(i, i+1));
 	}
-//i번째 글자가 자음인지 //a,e,i,o,u 아닌 나머지 if문 사용
-public boolean isConsonant(int i) {
-    //변수선언
-    String vowel = "aeiou";
-    boolean conso = true;
-    if (vowel.contains(letters.substring(i, i + 1))) {
-        conso = false;
-    }
-    return conso;
+		
+	// i번째 글자가 자음(a,e,i,o,u 제외한 나머지)인지 
+	public boolean isConsonant(int i) {
+		return !isVowel(i);
 }
 }
